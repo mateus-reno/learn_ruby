96081c9d07aa847b9f4c5321d97b9469b505db63
Mateus Kratz <mateuskratz@gmail.com>
2017-11-07 14:17:34 +0200

    Solved 'simon says' problem

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,32 @@
+def echo(parameter)
+  return parameter
+end
+
+def shout(parameter)
+  return parameter.upcase
+end
+
+def repeat(parameter, times=2)
+  return ("#{parameter} " * times).strip
+end
+
+def start_of_word(parameter, index=1)
+  return parameter[0..(index-1)]
+end
+
+def first_word(parameter)
+  return parameter.split[0]
+end
+
+def titleize(parameter)
+  exception = ["and", "over", "the"]
+  parameter.split.map.with_index do |word, i|
+    if (exception.include? word) && (i != 0)
+      word
+    else
+      word.capitalize
+    end
+  end.join(" ")
+end
+
 #write your code here
