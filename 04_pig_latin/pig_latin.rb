ceb1fe25ab4dd925a7020037322bb67ee8decef2
Mateus Kratz <mateuskratz@gmail.com>
2017-11-07 18:52:18 +0200

    WIP on 'pig latin' problem

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,22 @@
+def translate (word)
+  exception = ["a", "e", "i", "u", "o"]
+  if word.match(" ")
+    puts "buh"
+  else
+    wordarray = word.split("")
+    letter1 = word[0]
+    letter2 = word[1]
+    if exception.include? wordarray[0]
+      "#{word}" + "ay"
+    else
+      if exception.include? wordarray[1]
+        word[0] = ""
+        "#{word}" + "#{letter1}" + "ay"
+      else
+        word[0..1] = ""
+        "#{word}" + "#{letter1}" + "#{letter2}" + "ay"
+      end
+    end
+  end
+end
 #write your code here
