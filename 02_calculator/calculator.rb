256b38495cbb576c01c6d4dd5a3d22b142ede69d
Mateus Kratz <mateuskratz@gmail.com>
2017-11-07 17:08:37 +0200

    Solved 'calculator' problem incl. bonus questions

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,37 @@
+def add (num1, num2)
+  num1 + num2
+end
+
+def subtract (num1, num2)
+  num1 - num2
+end
+
+def sum (array1)
+  array1.inject(0){|sum,x| sum + x }
+end
+
+def multiply (array1)
+  array1.inject(){|sum,x| sum * x }
+end
+
+def power (num1, num2)
+  num1**num2
+end
+
+def factorial (num1)
+  if num1 > 0
+    if num1 > 1
+      sum = 1
+      while num1 > 1
+        sum = sum * num1
+        num1 -= 1
+      end
+      return sum
+    else
+      1
+    end
+  else
+    0
+  end
+end
 #write your code here
