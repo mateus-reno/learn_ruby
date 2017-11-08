d4a234f652cae38db1f020bbae0d5775919611f4
Mateus Kratz <mateuskratz@gmail.com>
2017-11-09 00:06:25 +0200

    Solved the 'book' problem

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,13 @@
 class Book
-# write your code here
+  attr_accessor :title
+  EXCEPTION = %w(and in the of a an)
+  def title= (book)
+    @title = book.capitalize.split.map do |word|
+      if EXCEPTION.include? word
+        word
+      else
+        word.capitalize
+      end
+    end.join(" ")
+  end
 end
